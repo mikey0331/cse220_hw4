d5f5a376b4f042bb0e6336d7402c19160a4b9153
mikey0331 <haeseung.jung@stonybrook.edu>
2024-11-11 05:25:35 +0000

    commit

--- src/hw4.c
+++ src/hw4.c
@@ -24,7 +24,8 @@ typedef struct {
     int height;
     Player p1;
     Player p2;
-    int phase;  // 0=setup, 1=play
+    int phase;
+    int current_player;
 } GameState;
 
 void send_packet(int socket, const char *msg) {
@@ -33,52 +34,85 @@ void send_packet(int socket, const char *msg) {
     send(socket, buffer, strlen(buffer), 0);
 }
 
-int setup_listener(int port) {
-    int server_fd;
-    struct sockaddr_in address;
-    int opt = 1;
-    
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd < 0) return -1;
-    
-    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-    
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(port);
-    
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) return -1;
-    if (listen(server_fd, 1) < 0) return -1;
-    
-    return server_fd;
-}
+void handle_packet(GameState *game, char *packet, int player_num) {
+    Player *current = (player_num == 1) ? &game->p1 : &game->p2;
+    Player *other = (player_num == 1) ? &game->p2 : &game->p1;
 
-void handle_begin(GameState *game, char *packet, int player_socket, int is_player1) {
-    if (is_player1) {
-        int w, h;
-        if (sscanf(packet, "B %d %d", &w, &h) != 2 || w < 10 || h < 10) {
-            send_packet(player_socket, "E 200");
-            return;
-        }
-        game->width = w;
-        game->height = h;
-    } else if (strlen(packet) > 2) {
-        send_packet(player_socket, "E 200");
-        return;
+    switch(packet[0]) {
+        case 'F':
+            send_packet(current->socket, "H 0");
+            send_packet(other->socket, "H 1");
+            break;
+            
+        case 'B':
+            if (player_num == 1) {
+                int w, h;
+                if (sscanf(packet, "B %d %d", &w, &h) != 2 || w < 10 || h < 10) {
+                    send_packet(current->socket, "E 200");
+                    return;
+                }
+                game->width = w;
+                game->height = h;
+            }
+            send_packet(current->socket, "A");
+            break;
+            
+        case 'I':
+            if (!game->width || !game->height) {
+                send_packet(current->socket, "E 101");
+                return;
+            }
+            send_packet(current->socket, "A");
+            break;
+            
+        case 'S':
+            if (!current->initialized) {
+                send_packet(current->socket, "E 102");
+                return;
+            }
+            send_packet(current->socket, "A");
+            break;
+            
+        case 'Q':
+            if (!current->initialized) {
+                send_packet(current->socket, "E 102");
+                return;
+            }
+            send_packet(current->socket, "A");
+            break;
+            
+        default:
+            send_packet(current->socket, "E 100");
     }
-    send_packet(player_socket, "A");
 }
 
 int main() {
     GameState game = {0};
-    int server1_fd = setup_listener(PORT1);
-    int server2_fd = setup_listener(PORT2);
+    int server1_fd = socket(AF_INET, SOCK_STREAM, 0);
+    int server2_fd = socket(AF_INET, SOCK_STREAM, 0);
     
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
+    struct sockaddr_in addr1 = {0}, addr2 = {0};
+    int opt = 1;
     
-    game.p1.socket = accept(server1_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
-    game.p2.socket = accept(server2_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+    setsockopt(server1_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    setsockopt(server2_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    
+    addr1.sin_family = AF_INET;
+    addr1.sin_addr.s_addr = INADDR_ANY;
+    addr1.sin_port = htons(PORT1);
+    
+    addr2.sin_family = AF_INET;
+    addr2.sin_addr.s_addr = INADDR_ANY;
+    addr2.sin_port = htons(PORT2);
+    
+    bind(server1_fd, (struct sockaddr *)&addr1, sizeof(addr1));
+    bind(server2_fd, (struct sockaddr *)&addr2, sizeof(addr2));
+    
+    listen(server1_fd, 1);
+    listen(server2_fd, 1);
+    
+    game.p1.socket = accept(server1_fd, NULL, NULL);
+    game.p2.socket = accept(server2_fd, NULL, NULL);
     
     char buffer[BUFFER_SIZE];
     fd_set readfds;
@@ -88,30 +122,22 @@ int main() {
         FD_SET(game.p1.socket, &readfds);
         FD_SET(game.p2.socket, &readfds);
         
-        int max_sd = (game.p1.socket > game.p2.socket) ? game.p1.socket : game.p2.socket;
+        int maxfd = (game.p1.socket > game.p2.socket) ? game.p1.socket : game.p2.socket;
         
-        select(max_sd + 1, &readfds, NULL, NULL, NULL);
+        select(maxfd + 1, &readfds, NULL, NULL, NULL);
         
         if (FD_ISSET(game.p1.socket, &readfds)) {
             memset(buffer, 0, BUFFER_SIZE);
-            if (read(game.p1.socket, buffer, BUFFER_SIZE) <= 0) {
-                send_packet(game.p2.socket, "H 1");
-                send_packet(game.p1.socket, "H 0");
-                break;
-            }
+            if (read(game.p1.socket, buffer, BUFFER_SIZE) <= 0) break;
             buffer[strcspn(buffer, "\n")] = 0;
-            handle_begin(&game, buffer, game.p1.socket, 1);
+            handle_packet(&game, buffer, 1);
         }
         
         if (FD_ISSET(game.p2.socket, &readfds)) {
             memset(buffer, 0, BUFFER_SIZE);
-            if (read(game.p2.socket, buffer, BUFFER_SIZE) <= 0) {
-                send_packet(game.p1.socket, "H 1");
-                send_packet(game.p2.socket, "H 0");
-                break;
-            }
+            if (read(game.p2.socket, buffer, BUFFER_SIZE) <= 0) break;
             buffer[strcspn(buffer, "\n")] = 0;
-            handle_begin(&game, buffer, game.p2.socket, 0);
+            handle_packet(&game, buffer, 2);
         }
     }
     
