0767838d2827e7821a8e27390b4a423be4133159
mikey0331 <haeseung.jung@stonybrook.edu>
2024-11-15 23:49:41 +0000

    commit

--- src/hw4.c
+++ src/hw4.c
@@ -284,32 +284,50 @@ int main() {
     game.phase = 0;
     game.current_turn = 1;
     
+    // Create sockets with network enabled
     int server1_fd = socket(AF_INET, SOCK_STREAM, 0);
     int server2_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server1_fd < 0 || server2_fd < 0) {
+        perror("Socket creation failed");
+        exit(1);
+    }
     
-    struct sockaddr_in addr1 = {0}, addr2 = {0};
+    // Enable socket reuse
     int opt = 1;
-    
     setsockopt(server1_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
     setsockopt(server2_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
     
+    // Configure network addresses
+    struct sockaddr_in addr1 = {0}, addr2 = {0};
+    
     addr1.sin_family = AF_INET;
-    addr1.sin_addr.s_addr = INADDR_ANY;
-    addr1.sin_port = htons(PORT1);
+    addr1.sin_addr.s_addr = INADDR_ANY;  // Accept connections from any interface
+    addr1.sin_port = htons(2201);        // Player 1 port
     
     addr2.sin_family = AF_INET;
-    addr2.sin_addr.s_addr = INADDR_ANY;
-    addr2.sin_port = htons(PORT2);
+    addr2.sin_addr.s_addr = INADDR_ANY;  // Accept connections from any interface
+    addr2.sin_port = htons(2202);        // Player 2 port
     
-    bind(server1_fd, (struct sockaddr *)&addr1, sizeof(addr1));
-    bind(server2_fd, (struct sockaddr *)&addr2, sizeof(addr2));
+    // Bind with network enabled
+    if (bind(server1_fd, (struct sockaddr *)&addr1, sizeof(addr1)) < 0) {
+        perror("Bind failed for player 1");
+        exit(1);
+    }
     
+    if (bind(server2_fd, (struct sockaddr *)&addr2, sizeof(addr2)) < 0) {
+        perror("Bind failed for player 2");
+        exit(1);
+    }
+    
+    // Listen for network connections
     listen(server1_fd, 1);
     listen(server2_fd, 1);
     
+    // Accept network connections
     game.p1.socket = accept(server1_fd, NULL, NULL);
     game.p2.socket = accept(server2_fd, NULL, NULL);
     
+    // Game loop with network I/O
     char buffer[BUFFER_SIZE];
     fd_set readfds;
     
@@ -321,6 +339,7 @@ int main() {
         int maxfd = (game.p1.socket > game.p2.socket) ? game.p1.socket : game.p2.socket;
         select(maxfd + 1, &readfds, NULL, NULL, NULL);
         
+        // Handle network data from Player 1
         if(FD_ISSET(game.p1.socket, &readfds)) {
             memset(buffer, 0, BUFFER_SIZE);
             ssize_t bytes = read(game.p1.socket, buffer, BUFFER_SIZE-1);
@@ -330,6 +349,7 @@ int main() {
             process_packet(&game, buffer, 1);
         }
         
+        // Handle network data from Player 2
         if(FD_ISSET(game.p2.socket, &readfds)) {
             memset(buffer, 0, BUFFER_SIZE);
             ssize_t bytes = read(game.p2.socket, buffer, BUFFER_SIZE-1);
@@ -340,6 +360,7 @@ int main() {
         }
     }
     
+    // Clean up network connections
     close(game.p1.socket);
     close(game.p2.socket);
     close(server1_fd);
